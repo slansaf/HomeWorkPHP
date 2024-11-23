1de9dc061c60649fdbb1b53aabc49f7a4fdc56c3
admin <ruslan@Ubuntu22.04.myguest.virtualbox.org>
2024-11-23 12:14:03 +0300

    A new repository with PHP homework has been created.

---
+++ HomeWork_1/php-cli/start.php
@@ -0,0 +1,3 @@
+<?php
+echo "Привет, GeekBrains \n";
+// docker run --rm -v ${pwd}/php-cli/:/cli php:8.2-cli php /cli/start.php
---
+++ HomeWork_1/php-fpm/code/index.php
@@ -0,0 +1,6 @@
+<?php
+
+echo "Привет, GeekBrains!<br>".date("Y-m-d H:i:s") ."<br><br>#";
+
+echo "Что-то еще 123456";
+?>
